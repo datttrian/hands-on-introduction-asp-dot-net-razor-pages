3be091609bf7bd286a006d486768e1637e77b0a2
datttrian <datttrian@gmail.com>
2023-10-07 19:43:21 +0000

    Write code in the PageModel class

--- src/BigStar/Pages/ShowDates.cshtml.cs
+++ src/BigStar/Pages/ShowDates.cshtml.cs
@@ -5,8 +5,26 @@ namespace MyApp.Namespace
 {
     public class ShowDatesModel : PageModel
     {
+        public IEnumerable<DateOnly> SaleDates { get; set; }
         public void OnGet()
         {
+            SaleDates = GetSaleDates(DateOnly.FromDateTime(DateTime.Now));
+        }
+        private IEnumerable<DateOnly> GetSaleDates(DateOnly startingDate)
+        {
+            var allDays = AllDatesInMonth(startingDate.Year, startingDate.Month);
+            var SaleDays = allDays.Where(x =>
+                x.DayOfWeek == DayOfWeek.Monday ||
+                x.DayOfWeek == DayOfWeek.Wednesday);
+            return SaleDays;
+        }
+        public IEnumerable<DateOnly> AllDatesInMonth(int year, int month)
+        {
+            int days = DateTime.DaysInMonth(year, month);
+            for (int day = 1; day <= days; day++)
+            {
+                yield return new DateOnly(year, month, day);
+            }
         }
     }
 }
