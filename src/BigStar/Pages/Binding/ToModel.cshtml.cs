2069a5100393b49f95190192edaed88e9f07a6b7
datttrian <datttrian@gmail.com>
2023-10-08 19:10:16 +0000

    Model binding into the model class

--- src/BigStar/Pages/Binding/ToModel.cshtml.cs
+++ src/BigStar/Pages/Binding/ToModel.cshtml.cs
@@ -3,8 +3,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BigStar.Pages.Binding
 {
+    [BindProperties(SupportsGet = true)]
     public class ToModelModel : PageModel
     {
+        public string CardName { get; set; }
+        public decimal? OriginalPrice { get; set; }
+        public BigStar.Models.CollectibleCard CurrentCard { get; set; }
         public void OnGet()
         {
         }
