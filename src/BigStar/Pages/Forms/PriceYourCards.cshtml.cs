d69fe42799d5f23ba441301e70bdeeed4e7653c8
datttrian <datttrian@gmail.com>
2023-10-08 20:33:33 +0000

    Add SelectList code

--- src/BigStar/Pages/Forms/PriceYourCards.cshtml.cs
+++ src/BigStar/Pages/Forms/PriceYourCards.cshtml.cs
@@ -1,6 +1,7 @@
-using BigStar.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BigStar.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BigStar.Pages.Forms
 {
@@ -9,14 +10,26 @@ namespace BigStar.Pages.Forms
 	{
 		public TradeCard Card { get; set; }
 		public string Message { get; set; } = "No card found!";
+		public List<SelectListItem> CardItems { get; set; }
+		public String CardName { get; set; }
 
+		public void SetupLists()
+		{
+			var orderedCards = new CardSource().CardsOrderedByName;
+			CardItems = orderedCards.Select(x => new SelectListItem(text: x.CardName, value: x.CardName)).ToList();
+		}
 		public void OnGet()
 		{
+			SetupLists();
 		}
 		public void OnPost()
 		{
-			var offerPrice = Card.CardCount * 12.50;
-			Message = $"Our offer for {Card.CardCount} {Card.CardName} card(s) in {Card.Condition} condition = {offerPrice:C} USD";
+			SetupLists();
+
+			// var offerPrice = 12.50 * Card.CardCount;
+
+			var offerPrice = CardSource.GetOfferPrice(CardName, Condition.Good);
+			Message = $"Our offer for {Card.CardCount} {CardName} card(s) in {Card.Condition} condition = {(offerPrice):C} USD ";
 		}
 	}
 }
