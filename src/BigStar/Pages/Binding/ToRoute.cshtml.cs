dd9fd890d0f3c1c67b3a4848398ebc22c4f4f13a
datttrian <datttrian@gmail.com>
2023-10-08 18:20:19 +0000

    Use routes with model binding

---
+++ src/BigStar/Pages/Binding/ToRoute.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BigStar.Pages.Binding
+{
+	[BindProperties(SupportsGet = true)]
+	public class ToRouteModel : PageModel
+	{
+		public string CardName { get; set; }
+		public decimal OriginalPrice { get; set; }
+		public void OnGet()
+		{
+		}
+	}
+}
