ac0bcb43ad018fc9a8802a87a642ec5bdd550654
datttrian <datttrian@gmail.com>
2023-10-08 20:06:55 +0000

    Add model and form code

---
+++ src/BigStar/Models/TradeCard.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+namespace BigStar.Models
+{
+    public class TradeCard
+    {
+        public int CardCount { get; set; } = 1;
+        public string CardName { get; set; }
+        public Condition Condition { get; set; } = Condition.Mint;
+    }
+}
